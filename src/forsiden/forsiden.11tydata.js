b9b90c76930086379a3413e39800f64b3c10dc55
Martin Berglund <martin.berglund@nav.no>
2022-08-02 15:42:42 +0200

    Testing frontpage animations

--- src/forsiden/forsiden.11tydata.js
+++ src/forsiden/forsiden.11tydata.js
@@ -1,98 +1,47 @@
 module.exports = {
-  importantLinks: [
+  servicesList: [
     {
-      image: "/static/images/coronavirus.svg",
-      title: "Koronavirus – hva gjelder i min situasjon?",
+      title: 'Arbeid',
+      image: "/static/images/coronavirus.svg"
     },
     {
-      image: "/static/images/globe.svg",
-      title: "EØS-saken: Informasjon og status",
+      title: 'Helse og sykdom',
+      image: "/static/images/coronavirus.svg"
     },
+    {
+      title: 'Familie og barn',
+      image: "/static/images/coronavirus.svg"
+    },
+    {
+      title: 'Pensjon',
+      image: "/static/images/coronavirus.svg"
+    },
+    {
+      title: 'Sosiale tjenester og veiledning',
+      image: "/static/images/coronavirus.svg"
+    },
+    {
+      title: 'Hjelpemidler og tilrettelegging',
+      image: "/static/images/coronavirus.svg"
+    }
   ],
-  promotedLinks: [
+  shortcutsList: [
     {
-      image: "/static/images/login.svg",
-      title: "Logg inn på Ditt NAV",
-      description: "Se dine utbetalinger, meldinger, saker, sykefravær m.m.",
+      title: 'Saksbehandlingstider'
     },
     {
-      image: "/static/images/helmet.svg",
-      title: "Ledige jobber",
-      description: "Finn ledige stillinger og registrer CV",
+      title: 'Utbetalingsdatoer'
     },
     {
-      image: "/static/images/fill-forms.svg",
-      title: "Skjema og søknad",
-      description: "Skjemaer for å søke og ettersende vedlegg",
-    },
+      title: 'Pengestøtter og tjenester fra A til Å'
+    }
   ],
-  situationLinks: [
+  latestList: [
     {
-      image: "/static/images/bag.svg",
-      title: "Arbeidssøker eller permittert",
-      description:
-        "Om jobb, registrering, CV, dagpenger og feriepenger av dagpenger",
+      title: 'Feriepenger fra NAV'
     },
     {
-      image: "/static/images/saving.svg",
-      title: "Pensjon",
-      description:
-        "Om alderspensjon, beregne og søke pensjon, AFP, utland og pensjon",
-    },
-    {
-      image: "/static/images/heart.svg",
-      title: "Venter barn",
-      description: "Om foreldrepenger, engangsstønad og svangerskapspenger",
-    },
-    {
-      image: "/static/images/social_aid.svg",
-      title: "Alene med barn",
-      description:
-        "Om støtteordninger for deg som er alene med barn fra fødsel eller ved samlivsbrudd",
-    },
-    {
-      image: "/static/images/hand_bandage.svg",
-      title: "Sykmeldt",
-      description: "Om sykepenger, sykmelding og hvordan komme tilbake i jobb",
-    },
-    {
-      image: "/static/images/fill-forms.svg",
-      title: "Skal søke AAP",
-      description:
-        "Om arbeidsavklaringspenger og overgang fra sykepenger til AAP",
-    },
-    {
-      image: "/static/images/health-case.svg",
-      title: "Har sykdom i familien",
-      description: "Om pleiepenger, opplæringspenger og omsorgspenger",
-    },
-    {
-      image: "/static/images/guide_dog.svg",
-      title: "Trenger tilrettelegging",
-      description:
-        "Om hjelpemidler, tilrettelegging, tolk og bil – i arbeid og fritid",
-    },
-    {
-      image: "/static/images/money.svg",
-      title: "Trenger økonomisk sosialhjelp",
-      description:
-        "Om økonomisk sosialhjelp, nødhjelp og flere sosiale tjenester",
-    },
-    {
-      image: "/static/images/calculator.svg",
-      title: "Trenger økonomisk rådgivning",
-      description: "Om økonomisk rådgivning og gjeldsrådgivning",
-    },
-    {
-      image: "/static/images/fill-forms.svg",
-      title: "Berørt av EØS-saken",
-      description: "Om behandling av EØS-sakene",
-    },
-    {
-      image: "/static/images/flower.svg",
-      title: "Har mistet noen i nær familie",
-      description:
-        "Om ytelser til gjenlevende, barnepensjon, gravferdsstønad med flere",
-    },
-  ],
+      title: 'Pensjonister kan ha fått feil skattetrekk'
+    }
+  ]
 };
