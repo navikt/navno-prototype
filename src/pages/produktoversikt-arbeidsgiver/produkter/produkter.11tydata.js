3df142a4e728f02e64f413c4eb147475da49028a
Martin Berglund <martin.berglund@nav.no>
2023-01-23 14:52:37 +0100

    Update produkter.11tydata.js

--- src/pages/produktoversikt-arbeidsgiver/produkter/produkter.11tydata.js
+++ src/pages/produktoversikt-arbeidsgiver/produkter/produkter.11tydata.js
@@ -1,4 +1,24 @@
 module.exports = {
   permalink: false,
   tags: ["produktArb"],
+  produkter: [
+    {
+      title: "Eksempel 3",
+      ingress: "Ingress her …",
+      type: ["Pengestøtte"],
+      area: ["Helse og sykdom"],
+    },
+    {
+      title: "Eksempel 3",
+      ingress: "Ingress her …",
+      type: ["Pengestøtte"],
+      area: ["Helse og sykdom"],
+    },
+    {
+      title: "Eksempel 3",
+      ingress: "Ingress her …",
+      type: ["Pengestøtte"],
+      area: ["Helse og sykdom"],
+    },
+  ],
 };
