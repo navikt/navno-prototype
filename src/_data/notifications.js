569216d531b05a6261eeaeffea90e6001d99c549
Martin Berglund <martin.berglund@nav.no>
2023-04-04 13:01:41 +0200

    Oppsett for nytt menyforslag basert på min-side-skisser

--- src/_data/notifications.js
+++ src/_data/notifications.js
@@ -1,78 +1,56 @@
 module.exports = {
   myTasks: [
     {
-      title: "Les vedtak",
-      date: "23. jul.",
-      description: "Du må lese vedtaket på søknaden din om dagpenger.",
+      title: "Du har motatt et vedtak på søknaden din om foreldrepenger",
+      timestamp: "I går, kl 16.48",
       tags: [
         {
-          title: "Varslet på e-post",
-        },
-        {
-          color: "bg-orange-100",
-          title: "Frist 6.8.2022",
+          title: "Varslet på SMS",
         },
       ],
     },
     {
-      title: "Last opp vedlegg",
-      date: "5. jul.",
-      description:
-        "Vi mangler vedlegg for å kunne behandle søknaden din om dagpenger.",
-      tags: [{ title: "Varslet på SMS" }],
+      title:
+        "Vi mangler vedlegg for å kunne behandle søknaden din om dagpenger",
+      timestamp: "16. mars 2023, kl 16.48",
+      tags: [{ title: "Varslet på SMS og e-post" }],
     },
   ],
-  myInbox: [
+  myMessages: [
     {
       title: "Familie – Samtalereferat",
-      date: "15. aug.",
-      description: "Foreldrepenger – Surrogati: Fremgan…",
-      color: "bg-green-300",
+      timestamp: "15. aug.",
     },
     {
       title: "Familie – Samtalereferat",
-      date: "14. jul.",
-      description: "Hei! Vi mottok den 3.7.19 din søknad…",
-      color: "bg-green-300",
+      timestamp: "14. jul.",
     },
     {
       title: "Regnskap/utbetaling – Årsop…",
-      date: "10. jan.",
-      description: "Du har fått årsoppgave fra NAV over dine…",
+      timestamp: "10. jan.",
       tags: ["Åpnes i regnskap/utbetaling"],
-      color: "bg-red-300",
     },
     {
       title: "Familie – Samtalereferat",
-      date: "15. aug.",
-      description: "Foreldrepenger – Surrogati: Fremgan…",
-      color: "",
+      timestamp: "15. aug.",
     },
     {
       title: "Familie – Samtalereferat",
-      date: "14. jul.",
-      description: "Hei! Vi mottok den 3.7.19 din søknad…",
-      color: "",
+      timestamp: "14. jul.",
     },
     {
       title: "Regnskap/utbetaling – Årsop…",
-      date: "10. jan.",
-      description: "Du har fått årsoppgave fra NAV over dine…",
+      timestamp: "10. jan.",
       tags: ["Åpnes i regnskap/utbetaling"],
-      color: "",
     },
     {
       title: "Familie – Samtalereferat",
-      date: "14. jul.",
-      description: "Hei! Vi mottok den 3.7.19 din søknad…",
-      color: "",
+      timestamp: "14. jul.",
     },
     {
       title: "Regnskap/utbetaling – Årsop…",
-      date: "10. jan.",
-      description: "Du har fått årsoppgave fra NAV over dine…",
+      timestamp: "10. jan.",
       tags: ["Åpnes i regnskap/utbetaling"],
-      color: "",
     },
   ],
 };
