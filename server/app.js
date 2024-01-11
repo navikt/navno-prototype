d1f3c1d53838d3c948e2c76d0fe0f680d944bcc7
Martin Berglund <martin.berglund@nav.no>
2024-01-11 15:07:40 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,13 +10,13 @@ app.set("port", 3123);
 app.use(compression());
 
 // Use Express to serve the static assets
-app.use("/person/prototype", express.static(path.join(__dirname, "../build")));
+app.use("/prototype", express.static(path.join(__dirname, "../build")));
 
-app.get("/person/prototype/internal/isAlive", (req, res) => {
+app.get("/prototype/internal/isAlive", (req, res) => {
   return res.status(200).send("I'm alive!");
 });
 
-app.get("/person/prototype/internal/isReady", (req, res) => {
+app.get("/prototype/internal/isReady", (req, res) => {
   return res.status(200).send("I'm ready!");
 });
 
