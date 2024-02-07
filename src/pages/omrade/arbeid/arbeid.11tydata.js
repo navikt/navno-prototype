e04566e5c2692f76910706b49076bd8f64eb20b1
Martin Berglund <martin.berglund@nav.no>
2024-02-07 15:24:11 +0100

    Flere områdeside-eksempler

---
+++ src/pages/omrade/arbeid/arbeid.11tydata.js
@@ -0,0 +1,67 @@
+module.exports = {
+  situationList: [
+    {
+      title: "Er arbeidsledig eller permittert",
+      ingress:
+        "Om pengestøtte og hjelp fra NAV når du har mistet jobben eller er permittert.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Søker jobb",
+      ingress:
+        "Jobbsøkertips og ledige jobber når du er arbeidssøker eller ønsker å skifte jobb.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Trenger hjelp til å komme i jobb",
+      ingress: "NAV har tilbud når du trenger hjelp til å komme i jobb.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Trenger hjelp til å fullføre utdanning",
+      ingress:
+        "Hjelp og støtte når du har avbrutt utdanning eller har utfordringer med å gjennomføre skolegangen.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Trenger tilrettelegging på jobb eller i utdanning",
+      ingress:
+        "Om hjelpemidler og tilrettelegging på arbeidsplassen eller skolen for å kunne jobbe eller gjennomføre utdanning.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Kan bare jobbe noe på grunn av langvarig sykdom eller skade",
+      ingress:
+        "Om muligheter til å komme i jobb eller aktivitet og pengestøtter som sykepenger, AAP, uføretrygd med flere.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Har vært syk eller skadet lenge",
+      ingress:
+        "Om støtteordninger og veien videre når du er usikker på hvor mye du kan jobbe fremover.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Er utenlandsk arbeidstaker i Norge",
+      ingress: "Om hvilke trygderegler som gjelder når du skal jobbe i Norge.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Er berørt av krigen i Ukraina ",
+      ingress: "Du kan få hjelp fra NAV når du er flyktning.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Skal jobbe i utlandet",
+      ingress:
+        "Om hvilke trygderegler som gjelder når du skal jobbe i utlandet.",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Skal oppholde deg i utlandet",
+      ingress:
+        "Om hvilke trygderegler som gjelder når du skal oppholde deg i utlandet.",
+      category: "Dette kan du ha rett til",
+    },
+  ],
+};
---
+++ src/pages/omrade/helse-og-sykdom/helse-og-sykdom.11tydata.js
@@ -0,0 +1,53 @@
+module.exports = {
+  situationList: [
+    {
+      title: "Har blitt sykmeldt",
+      ingress: "Om sykepenger, hvilke regler som gjelder og hva du må gjøre.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/har-blitt-sykemeldt.svg",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Har vært syk eller skadet lenge",
+      ingress:
+        "Om støtteordninger og veien videre når du er usikker på hvor mye du kan jobbe fremover.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/har-vaert-syk-eller-skadet-lenge.svg",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Kan bare jobbe noe på grunn av langvarig sykdom eller skade",
+      ingress:
+        "Om muligheter til å komme i jobb eller aktivitet og pengestøtter som sykepenger, AAP, uføretrygd med flere.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/kan-bare-jobbe-noe-paa-grunn-av-langvarig-sykdom-eller-skade.svg",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Har en varig sykdom eller skade og kan ikke jobbe",
+      ingress:
+        "Om tilrettelegging, hjelpemidler og pengestøtter som sykepenger, AAP, uføretrygd med flere.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/har-varig-sykdom-eller-skade-og-kan-ikke-jobbe.svg",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Trenger pleie på grunn av sykdom eller skade",
+      ingress:
+        "Om støtteordninger når du eller barnet ditt har nødvendige ekstrautgifter eller behov for langvarig, privat pleie og tilsyn.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/trenger-pleie-paa-grunn-av-sykdom-eller-skade.svg",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title: "Tar vare på noen som er syk eller skadet",
+      ingress:
+        "Pengestøtte når du pleier et barn eller en voksen som er syk eller skadet.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/tar-vare-paa-noen-som-er-syk-eller-skadet.svg",
+      category: "Dette kan du ha rett til",
+    },
+    {
+      title:
+        "Har blitt skadet under arbeid, undervisning, rednings- eller militærtjeneste",
+      ingress:
+        "Du kan ha rettigheter når du har fått en yrkesskade eller en yrkessykdom.",
+      icon: "/src/_includes/svg/piktogrammer/situasjoner/har-blitt-skadet-under-arbeid-undervisning-rednings-ellermilitaertjeneste.svg",
+      category: "Dette kan du ha rett til",
+    },
+  ],
+};
