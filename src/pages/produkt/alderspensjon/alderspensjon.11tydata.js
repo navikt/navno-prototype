5564848518ccd75366749bfb81c91f68eccae6d9
Martin Berglund <martin.berglund@nav.no>
2024-02-29 14:20:40 +0100

    Excluded files

--- src/pages/produkt/alderspensjon/alderspensjon.11tydata.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  tags: ["alderspensjon"],
-  type: "Pengestøtte",
-  title: "Alderspensjon",
-  ingress:
-    "Alderspensjon fra folketrygden er livsvarig, og sikrer at du har inntekt når du er pensjonist.",
-  published: "Oppdatert 10. januar 2024",
-  alpine: true,
-};
