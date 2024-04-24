98d7452d23ccb7c7b7a0709f6a958caedbcf9561
Martin Berglund <martin.berglund@nav.no>
2024-04-24 11:47:08 +0200

    Oppdatert dato

--- src/pages/produkt/sykepenger/sykepenger.11tydata.js
+++ src/pages/produkt/sykepenger/sykepenger.11tydata.js
@@ -7,6 +7,6 @@ module.exports = {
     "Sykepenger erstatter inntekten din når du ikke kan jobbe på grunn av sykdom eller skade.",
   audience:
     "Det finnes også informasjon om sykepenger til [arbeidsgivere](#) og [leger og andre behandlere](#)",
-  published: "Oppdatert 26. januar 2024",
+  published: "Oppdatert 23. april 2024",
   alpine: true,
 };
