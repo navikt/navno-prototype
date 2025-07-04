87fadc0700de63c51e97f38fbc2295908a5f875d
Martin Berglund <martin.berglund@nav.no>
2025-07-04 13:37:25 +0200

    Lage min-side

---
+++ src/pages/min-side/min-side.11tydata.js
@@ -0,0 +1,45 @@
+export default {
+  sections: [
+    {
+      title: "Pengestøtte",
+      items: [
+        { text: "Dagpenger", icon: "/src/_includes/svg/icons/wallet.svg" },
+        { text: "Barnetrygd", icon: "/src/_includes/svg/icons/wallet.svg" },
+      ],
+    },
+    {
+      title: "Oppfølging",
+      items: [
+        {
+          text: "Dialog med veilederen din",
+          icon: "/src/_includes/svg/icons/chat2.svg",
+        },
+        {
+          text: "Aktiviteter, møter og samtalereferat",
+          icon: "/src/_includes/svg/icons/suitcase.svg",
+        },
+      ],
+    },
+    {
+      title: "På tvers",
+      items: [
+        {
+          text: "Din profil",
+          icon: "/src/_includes/svg/icons/person.svg",
+        },
+        {
+          text: "Utbetalinger",
+          icon: "/src/_includes/svg/icons/wallet.svg",
+        },
+        {
+          text: "Dokumentarkiv",
+          icon: "/src/_includes/svg/icons/document.svg",
+        },
+        {
+          text: "Dialog med kontaktsenteret",
+          icon: "/src/_includes/svg/icons/chat2.svg",
+        },
+      ],
+    },
+  ],
+};
