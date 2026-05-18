fe97a3b4fcb31f5f2d0781b61443753c23c63e59
Martin Berglund <martin.berglund@nav.no>
2026-05-18 12:45:57 +0200

    Hurtigbytte representasjon

--- src/_data/site.js
+++ src/_data/site.js
@@ -21,19 +21,33 @@ export default {
   ],
   accounts: [
     {
+      type: "main",
       name: "Charlie Jensen",
-      role: "Deg selv",
+      role: "Din egen profil",
       icon: "/src/assets/images/icon-person.svg",
     },
     {
+      type: "agent",
       name: "Alex Jensen",
       role: "Du har foreldreansvar",
       icon: "/src/assets/images/icon-persongroup.svg",
     },
     {
+      type: "agent",
       name: "Casey Jensen",
       role: "Du har foreldreansvar",
       icon: "/src/assets/images/icon-persongroup.svg",
+    },
+  ],
+  notifications: [
+    {
+      title: "Du har mottatt et vedtak på søknaden din om foreldrepenger",
+      time: "13.10.23 kl. 15.23",
+      type: "Varslet på SMS",
+    },
+    {
+      title: "Vi har mottatt søknaden din om arbeidsavklaringspenger",
+      time: "13.10.23 kl. 15.23",
     },
   ],
 };
