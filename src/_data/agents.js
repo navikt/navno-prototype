177323f993e2ddd71e013362f51bbf669d4f8ad4
Martin Berglund <martin.berglund@nav.no>
2026-05-21 15:03:51 +0200

    Omstrukturering av data

---
+++ src/_data/agents.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    type: "main",
+    name: "Charlie Jensen",
+    role: "Din egen profil",
+    icon: "/src/assets/images/icon-person.svg",
+  },
+  {
+    type: "agent",
+    name: "Alex Jensen",
+    role: "Du har foreldreansvar",
+    icon: "/src/assets/images/icon-persongroup.svg",
+  },
+  {
+    type: "agent",
+    name: "Casey Jensen",
+    role: "Du har foreldreansvar",
+    icon: "/src/assets/images/icon-persongroup.svg",
+  },
+  {
+    type: "agent",
+    name: "Pippilotta Viktualia Rullgardina Krusmynte Efraimsdatter Langstrømpe",
+    role: "Du er verge",
+    icon: "/src/assets/images/icon-persongroup.svg",
+  },
+];
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,5 @@
 export default {
   title: "Prototype av enkelte nav.no-sider",
-  audience: ["Privatperson", "Arbeidsgiver", "Samarbeidspartner"],
   language: [
     {
       full: "Norsk bokmål",
@@ -17,43 +16,6 @@ export default {
     {
       full: "Davvisámegiella",
       short: "SMI",
-    },
-  ],
-  agents: [
-    {
-      type: "main",
-      name: "Charlie Jensen",
-      role: "Din egen profil",
-      icon: "/src/assets/images/icon-person.svg",
-    },
-    {
-      type: "agent",
-      name: "Alex Jensen",
-      role: "Du har foreldreansvar",
-      icon: "/src/assets/images/icon-persongroup.svg",
-    },
-    {
-      type: "agent",
-      name: "Casey Jensen",
-      role: "Du har foreldreansvar",
-      icon: "/src/assets/images/icon-persongroup.svg",
-    },
-    {
-      type: "agent",
-      name: "Pippilotta Viktualia Rullgardina Krusmynte Efraimsdatter Langstrømpe",
-      role: "Du er verge",
-      icon: "/src/assets/images/icon-persongroup.svg",
-    },
-  ],
-  notifications: [
-    {
-      title: "Du har mottatt et vedtak på søknaden din om foreldrepenger",
-      time: "13.10.23 kl. 15.23",
-      type: "Varslet på SMS",
-    },
-    {
-      title: "Vi har mottatt søknaden din om arbeidsavklaringspenger",
-      time: "13.10.23 kl. 15.23",
     },
   ],
 };
